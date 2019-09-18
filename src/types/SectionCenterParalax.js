4cab792f312d57cb005bdea367449aa9a8bba156
Alfa Irawan <alfa.irawan@outlook.com>
2019-09-18 07:43:04 +0700

    v0.1.12

--- src/types/SectionCenterParalax.js
+++ src/types/SectionCenterParalax.js
@@ -7,10 +7,33 @@ class SectionCenterParalax extends Component {
         super(props)
     };
 
+    handleScroll() {
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            document.getElementById("id-section-center-left-bottom-title-text-image-container").style.opacity = "0.6"
+        }
+        else {
+            document.getElementById("id-section-center-left-bottom-title-text-image-container").style.opacity = "0.8"
+        }
+    }
+
+    componentDidMount() {
+        if(window.innerWidth > 700) {
+            window.addEventListener("scroll", this.handleScroll);
+        }
+    }
+
+    componentWillUnmount() {
+        if(window.innerWidth > 700) {
+            window.removeEventListener("scroll", this.handleScroll);
+        }
+    }
+
     render() {
+        let windowsHeight = window.innerHeight;
+
         return(
-            <div id='id-section-center-left-bottom-title-container' className='section-center-left-bottom-title-container' style={{ zIndex: "-999", display: "flex", flexWrap: "unwrap", alignContent: 'center', alignItems: 'center', justifyContent: 'center' ,width: "100%", backgroundAttachment: "fixed", backgroundPosition: "center", backgroundRepeat: "no-repeat", backgroundImage: `url(${this.props.sectionBackgroundImage})`, backgroundSize: "cover" }}>
-                <div id='id-section-center-left-bottom-title-text-image-container' className='section-center-left-bottom-title-text-image-container' style={{ zIndex: "-99", display: "flex", flexWrap: "unwrap", alignContent: 'center', alignItems: 'center', justifyContent: 'center' ,width: "100%", backgroundColor: this.props.sectionBackgroundColor, opacity: '0.8' }}>
+            <div id='id-section-center-left-bottom-title-container' className='section-center-left-bottom-title-container' style={{ zIndex: "-999", display: "flex", flexWrap: "unwrap", alignContent: 'center', alignItems: 'center', justifyContent: 'center' ,width: "100%", height: windowsHeight, backgroundAttachment: "fixed", backgroundPosition: "center", backgroundRepeat: "no-repeat", backgroundImage: `url(${this.props.sectionBackgroundImage})`, backgroundSize: "cover" }}>
+                <div id='id-section-center-left-bottom-title-text-image-container' className='section-center-left-bottom-title-text-image-container' style={{ zIndex: "-99", display: "flex", flexWrap: "unwrap", alignContent: 'center', alignItems: 'center', justifyContent: 'center' ,width: "100%", height: windowsHeight, backgroundColor: this.props.sectionBackgroundColor, opacity: '0.8' }}>
 
                 </div>
             </div>
